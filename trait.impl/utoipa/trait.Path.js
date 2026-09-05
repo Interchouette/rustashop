15559060a12c38747acb06fb8724a70414677601
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-05 21:51:55 +0000

    Deploy documentation for 565cf7e26e2ffd30c4c3c9c5a81c9bdf0754cc8c

--- trait.impl/utoipa/trait.Path.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["rustashop_api",[["impl Path for <a class=\"struct\" href=\"rustashop_api/struct.list_admin_orders.html\" title=\"struct rustashop_api::list_admin_orders\">list_admin_orders</a>",0],["impl Path for <a class=\"struct\" href=\"rustashop_api/struct.list_admin_products.html\" title=\"struct rustashop_api::list_admin_products\">list_admin_products</a>",0],["impl Path for <a class=\"struct\" href=\"rustashop_api/struct.openapi_json.html\" title=\"struct rustashop_api::openapi_json\">openapi_json</a>",0],["impl Path for <a class=\"struct\" href=\"rustashop_api/struct.patch_admin_order.html\" title=\"struct rustashop_api::patch_admin_order\">patch_admin_order</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[670]}

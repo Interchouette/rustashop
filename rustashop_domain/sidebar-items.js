2b6561e0acc90596f1b5d6d19efd7692927de057
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-06 23:47:25 +0000

    Deploy documentation for 00f24471d1e44f2ef7f0e1a6f5e45cf3ea19a732

--- rustashop_domain/sidebar-items.js
+++ rustashop_domain/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["PAYMENT_STATUS_PENDING"],"enum":["CartStatus","DomainError","OrderState"],"struct":["Cart","CartLine","Category","Currency","Money","Order","OrderLine","Product","ProductVariant"]};
+window.SIDEBAR_ITEMS = {"constant":["PAYMENT_STATUS_PENDING"],"enum":["CartStatus","DomainError","OrderState"],"struct":["Cart","CartLine","Category","Currency","Money","Order","OrderLine","Product","ProductVariant"],"trait":["CartRepository","CategoryRepository","ProductRepository"]};
--- trait.impl/serenade_contracts/product/trait.ProductRepository.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["rustashop_persist_seaorm",[["impl ProductRepository for <a class=\"struct\" href=\"rustashop_persist_seaorm/catalog/struct.SeaOrmCatalogRepository.html\" title=\"struct rustashop_persist_seaorm::catalog::SeaOrmCatalogRepository\">SeaOrmCatalogRepository</a>",0]]],["rustashop_persist_sqlx",[["impl ProductRepository for <a class=\"struct\" href=\"rustashop_persist_sqlx/catalog/struct.SqlxCatalogRepository.html\" title=\"struct rustashop_persist_sqlx::catalog::SqlxCatalogRepository\">SqlxCatalogRepository</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[265,254]}

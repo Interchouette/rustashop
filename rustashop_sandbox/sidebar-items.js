289c383c4707153fa4117b849ac6c2fd385593ba
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-07 17:34:46 +0000

    Deploy documentation for 23fbec8d4541766ea6365877b32169a947060be4

---
+++ rustashop_sandbox/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["PYTHON_PACKAGE_URL"],"fn":["apply_validated_adjustments","invoke_python_quote","quote_fixture_source","validate_adjustments"],"struct":["Adjustment","CartLine","CartSnapshot","Money"]};

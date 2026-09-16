cad3f1c3ddc8068c947567a1dc1650d5eae750bd
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-16 17:00:04 +0000

    Deploy documentation for b76999e73fc53c4799d1961bfeace8e0464e2c54

--- trait.impl/rustashop_domain/repositories/trait.CategoryRepository.js
+++ trait.impl/rustashop_domain/repositories/trait.CategoryRepository.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["rustashop_persist_seaorm",[["impl CategoryRepository for <a class=\"struct\" href=\"rustashop_persist_seaorm/catalog/struct.SeaOrmCatalogRepository.html\" title=\"struct rustashop_persist_seaorm::catalog::SeaOrmCatalogRepository\">SeaOrmCatalogRepository</a>",0]]],["rustashop_persist_sqlx",[["impl CategoryRepository for <a class=\"struct\" href=\"rustashop_persist_sqlx/catalog/struct.SqlxCatalogRepository.html\" title=\"struct rustashop_persist_sqlx::catalog::SqlxCatalogRepository\">SqlxCatalogRepository</a>",0]]]]);
+    const implementors = Object.fromEntries([["rustashop_persist_seaorm",[["impl <a class=\"trait\" href=\"rustashop_domain/repositories/trait.CategoryRepository.html\" title=\"trait rustashop_domain::repositories::CategoryRepository\">CategoryRepository</a> for <a class=\"struct\" href=\"rustashop_persist_seaorm/catalog/struct.SeaOrmCatalogRepository.html\" title=\"struct rustashop_persist_seaorm::catalog::SeaOrmCatalogRepository\">SeaOrmCatalogRepository</a>",0]]],["rustashop_persist_sqlx",[["impl <a class=\"trait\" href=\"rustashop_domain/repositories/trait.CategoryRepository.html\" title=\"trait rustashop_domain::repositories::CategoryRepository\">CategoryRepository</a> for <a class=\"struct\" href=\"rustashop_persist_sqlx/catalog/struct.SqlxCatalogRepository.html\" title=\"struct rustashop_persist_sqlx::catalog::SqlxCatalogRepository\">SqlxCatalogRepository</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[266,255]}
+//{"start":59,"fragment_lengths":[425,414]}

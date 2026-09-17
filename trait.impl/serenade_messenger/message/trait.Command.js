b5489592f6e9c11640292f37d66a748cb3a06472
Interchouette <484423+Interchouette@users.noreply.github.com>
2026-09-17 09:08:01 +0000

    Deploy documentation for 7b86d5555431ab04c5dae1d362d4e3353fc5e9c3

---
+++ trait.impl/serenade_messenger/message/trait.Command.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["rustashop_api",[["impl Command for <a class=\"enum\" href=\"rustashop_api/sandbox_messenger/enum.SandboxJobWork.html\" title=\"enum rustashop_api::sandbox_messenger::SandboxJobWork\">SandboxJobWork</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[209]}
